49024639c89607f99ab66df72da976d3225af77a
Jyotirmoy Bandyopadhyaya [Bravo68] <jbandyopadhayaya@gmail.com>
2023-12-09 18:54:21 +0530

    aoc: day 5 code refactoring

--- ts/05/code.ts
+++ ts/05/code.ts
@@ -1,43 +1,75 @@
 const pkg = await Bun.file(`${import.meta.dir}/package.json`).json();
 const input = await Bun.file(`${import.meta.dir}/../../input/5.txt`).text();
 
-import { Worker } from "worker_threads";
-
-import { parseInput, getSeedToLocationMapping } from "./helper.js";
-
-const lines = input.split("\n");
-const { seeds, mappings } = parseInput(lines);
-
-let minLocation = Infinity;
-
-for (const seed of seeds) {
-	const location = getSeedToLocationMapping(seed, mappings);
-	minLocation = Math.min(minLocation, location);
+const parse = (input: string) => {
+	const [seeds, ...sections] = input.split('\n\n').map((s) => s.trim())
+	return [
+	  seeds
+		.match(/seeds: ([\d ]*)/)![1]
+		.split(' ')
+		.map(Number),
+	  sections.map((s) =>
+		s
+		  .match(/\w+-to-\w+ map:([\d \n]*)/)![1]
+		  .trim()
+		  .split('\n')
+		  .map((row) => {
+			const [dest, source, range] = row.trim().split(' ').map(Number)
+			return {
+			  dest: [dest, dest + range + 1] as const,
+			  src: [source, source + range + 1] as const,
+			}
+		  }),
+	  ),
+	] as const
 }
 
-console.log("Day", pkg.name, "|", "Part 1 : ", minLocation);
+type Seed = ReturnType<typeof parse>[0][number]
+type Maps = ReturnType<typeof parse>[1]
 
-const chunks: number[][] = [];
-for (let i = 0; i < seeds.length - 1; i += 2) {
-  chunks.push([seeds[i], seeds[i + 1]]);
+const toLocation = (maps: Maps, value: Seed) =>
+	maps.reduce((curr, map) => {
+		for (const m of map) {
+			if (curr >= m.src[0] && curr < m.src[1])
+				return curr + m.dest[0] - m.src[0]
+		}
+		return curr
+	}, value)
+
+const toSeed = (maps: Maps, value: Seed) =>
+	maps.reduceRight((curr, map) => {
+		for (const element of map) {
+			const m = element
+			if (curr >= m.dest[0] && curr < m.dest[1])
+				return curr + m.src[0] - m.dest[0]
+		}
+		return curr
+	}, value)
+
+const solve = (seeds: Seed[], maps: Maps) => Math.min(...seeds.map((s) => toLocation(maps, s)))
+
+export const part1 = (input: string) => solve(...parse(input))
+
+const isValidSeed = (seeds: Seed[]) => (seed: Seed) => {
+	for (let i = 0; i < seeds.length; i += 2) {
+		const [a, b] = seeds.slice(i, i + 2)
+		if (seed >= a && seed < a + b) return true
+	}
+	return false
 }
 
-const tasks = chunks.map(
-  ([seedStart, seedRangeLength]): Promise<number> => {
-	return new Promise((resolve, reject) => {
-	  const worker = new Worker("./worker.js", {
-		workerData: { seedStart, seedRangeLength, mappings },
-	  });
-	  worker.on("message", resolve);
-	  worker.on("error", reject);
-	  worker.on("exit", (code) => {
-		if (code !== 0)
-		  reject(new Error(`Worker stopped with exit code ${code}`));
-	  });
-	});
-  }
-);
+export const part2 = (input: string) => {
+	const [seeds, maps] = parse(input)
 
-const minLocations = await Promise.all<number>(tasks);
+	const possibleSeeds = maps
+		.flatMap((m, i) =>
+		m.map(({ dest }) => toSeed(maps.slice(0, i + 1), dest[0])),
+		)
+		.filter(isValidSeed(seeds))
 
-console.log("Day", pkg.name, "|", "Part 2 : ", Math.min(...minLocations));
+	return solve(possibleSeeds, maps)
+}
+
+console.log("Day", pkg.name, "|", "Part 1 : ", part1(input));
+
+console.log("Day", pkg.name, "|", "Part 2 : ", part2(input));
